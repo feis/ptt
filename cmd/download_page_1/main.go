89a8d2961234cd6da79e2b18367caf826f82b01c
Ken-Yi Lee <kenyilee@gmail.com>
2019-03-15 12:58:36 +0800

    Update

---
+++ cmd/download_page_1/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://www.ptt.cc/bbs/movie/index.html")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = ioutil.WriteFile("index.html", b, 0644)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
---
+++ cmd/download_page_2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	resp, err := http.Get("https://www.ptt.cc/bbs/movie/index.html")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	doc.Find("link").Each(func(i int, s *goquery.Selection) {
+		if url, ok := s.Attr("href"); ok {
+			s.SetAttr("href", "https:"+url)
+		}
+	})
+
+	html, err := goquery.OuterHtml(doc.Selection)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = ioutil.WriteFile("index.html", []byte(html), 0644)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
