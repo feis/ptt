04170b35beeb3ed133854dd1a7f281153790870d
Ken-Yi Lee <kenyilee@gmail.com>
2019-03-17 10:50:50 +0800

    Update

--- cmd/download_page_2/main.go
+++ cmd/download_page_2/main.go
@@ -10,21 +10,17 @@ import (
 
 func main() {
 	resp, err := http.Get("https://www.ptt.cc/bbs/movie/index.html")
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	defer resp.Body.Close()
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	doc.Find("link").Each(func(i int, s *goquery.Selection) {
 		if url, ok := s.Attr("href"); ok {
 			s.SetAttr("href", "https:"+url)
@@ -32,12 +28,10 @@ func main() {
 	})
 
 	html, err := goquery.OuterHtml(doc.Selection)
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	err = ioutil.WriteFile("index.html", []byte(html), 0644)
 
 	if err != nil {
